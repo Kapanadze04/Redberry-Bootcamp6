d9deff9a1e69d8bb581bde4511e4b74ab1f49e66
kapanadze04 <kapanadze.gio96@gmail.com>
2024-09-17 20:50:21 +0400

    Add componenets

---
+++ src/app/experience/experience.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-experience',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './experience.component.html',
+  styleUrl: './experience.component.scss'
+})
+export class ExperienceComponent {
+
+}
---
+++ src/app/personal-info/personal-info.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+@Component({
+  selector: 'app-personal-info',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './personal-info.component.html',
+  styleUrl: './personal-info.component.scss'
+})
+export class PersonalInfoComponent {
+
+}
