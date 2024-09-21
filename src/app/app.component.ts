2291055f2e6f590cc796ad26b885dbe788d63417
kapanadze04 <kapanadze.gio96@gmail.com>
2024-09-21 14:06:58 +0400

    Home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PreviewComponent } from "./preview/preview.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PreviewComponent],
+  imports: [RouterOutlet, PreviewComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
 
 export const routes: Routes = [
-
+    { path: '', component: HomeComponent },
+    { path: 'personal-info', component: PersonalInfoComponent }
 ];
