e2cec0a7b7bb73a5b235a148202233bd69a32f51
kapanadze04 <kapanadze.gio96@gmail.com>
2024-10-03 19:49:31 +0400

    experience page, form markup

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderComponent } from "../shared/header/header.component";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-experience',
@@ -10,9 +11,13 @@ import { HeaderComponent } from "../shared/header/header.component";
   styleUrl: './experience.component.scss'
 })
 export class ExperienceComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private location: Location) {}
 
   onEducation() {
     this.router.navigate(['/education']);
+  }
+
+  onPreviousPage() {
+    this.location.back();
   }
 }
