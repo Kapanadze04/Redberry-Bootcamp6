9890d530f606f864cdad2455dd43e7c9cf288924
kapanadze04 <kapanadze.gio96@gmail.com>
2024-10-05 12:13:39 +0400

    education page, form markup

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderComponent } from "../shared/header/header.component";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-education',
@@ -10,5 +11,11 @@ import { HeaderComponent } from "../shared/header/header.component";
   styleUrl: './education.component.scss'
 })
 export class EducationComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private location: Location) {}
+
+  onPreviousPage() {
+    this.location.back();
+  }
+
+  onEducation() {}
 }
